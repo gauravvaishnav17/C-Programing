beeaf8b7c172a3336e3ec0f5e4373749f1b2e0e4
Gaurav Vaishnav <31757192+gauravvaishnav17@users.noreply.github.com>
2021-04-30 22:59:31 +0530

    Add files via upload

---
+++ rec_area.c
@@ -0,0 +1,13 @@
+//Finding area and perimeter of rectangle or circ
+#include<stdio.h>
+int main()
+{
+	int l,b;
+	printf("Enter the length of the rectangle\n");
+	scanf("%d",&l);
+	printf("\nEnter the breath of the rectangle\n");
+	scanf("%d",&b);
+	printf("area=%d\nperimeter=%d",l*b,2*(l+b));
+	
+		
+}
